6cbbbe02621e82e39a82d33422acb3b0ef3cbf27
ahmadhomsi <homsiahmad111@gmail.com>
2021-04-01 14:41:43 +0300

    we have new folder

---
+++ app.js
@@ -0,0 +1,13 @@
+alert("welcom in my wep for car ");
+alert("evrey thing about the can find it here ");
+var username =prompt ("plez enter your name ");
+document.write(username);
+var phonenuper =prompt ("plez enter your phonenuper");
+document.write(phonenuper);
+if (phonenuper > 0) {
+alert("good job");}
+else if (phonenuper <= 0) {
+    alert("not good ");}
+
+
+
