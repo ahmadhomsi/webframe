e535ebb5974b7876a4a306b2c5951d97f0bf6b37
ahmadhomsi <homsiahmad111@gmail.com>
2021-04-04 15:58:22 +0300

    add new file for work

--- app.js
+++ app.js
@@ -1,13 +1,42 @@
 alert("welcom in my wep for car ");
 alert("evrey thing about the can find it here ");
-var username =prompt ("plez enter your name ");
+var username = prompt("plez enter your  name ");
 document.write(username);
-var phonenuper =prompt ("plez enter your phonenuper");
+
+var phonenuper = prompt("plez enter your phonenuper");
 document.write(phonenuper);
+
 if (phonenuper > 0) {
-alert("good job");}
+    alert("good job");
+}
 else if (phonenuper <= 0) {
-    alert("not good ");}
+    alert("not good ");
+}
+
+// var cartyep = prompt('please enter your faverite car ');
+var cartyep = prompt('please enter your faverite car');
+
+while (cartyep !== 'sport' && cartyep !== 'economical'){
+
+    cartyep = prompt('please enter of the carplease choise sport or economical car');
+
+}
+var picNumber = prompt('please enter the number of photos you want to see');
+
+for (var i = 0; i <  picNumber; i++) {
+    if (cartyep == 'sport') {
+        document.write('<img src="https://www.goodcarbadcar.net/wp-content/uploads/2019/07/2019-audi-r8-onlocation.jpg"/>');
+    } else {
+        document.write('<img src="https://cnet4.cbsistatic.com/img/ekNnmq9C9nu6cxGEU5KPufzm8f4=/2019/05/13/461af4f8-c387-46a2-a3a6-c74e520f27ec/2019-toyota-prius-awd-e-35.jpg"/>')
+    }
+}
+
+function carlife (parm1, parm2 ){
+    for (var i = parm1; i < parm2; i++){
+    console.log (i);}
+    }
+
+    carlife(1,10); 
 
 
 
